b4c4671bd7c958538481c4afad8eb2520520ab0a
Pedro Prates <dev@pprates.com>
2026-08-27 14:18:05 +0100

    core: set game loop to 60 fps

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
         log_state()
 
@@ -20,6 +23,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
